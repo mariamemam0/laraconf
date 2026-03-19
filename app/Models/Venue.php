462d5a40e16f510ac03246196e0e414e4241258f
mariam <mariamemampp00@gmail.com>
2026-03-19 13:29:31 +0200

    upload image using spatie media with filament

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace App\Models;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
 use App\Enums\Region;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Venue extends Model
+class Venue extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected function casts(): array
     {
@@ -33,6 +37,10 @@ class Venue extends Model
                 Select::make('region')
                 ->enum(Region::class)
                 ->options(Region::class),
+                SpatieMediaLibraryFileUpload::make('images')
+                ->collection('venue-images')
+                ->multiple()
+                ->image(),
             ];
     }
 }
