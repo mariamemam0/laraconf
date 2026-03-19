67b2af644935cbc6f59657f8aa1b17c599a4d902
mariam <mariamemampp00@gmail.com>
2026-03-19 15:29:29 +0200

    update table columns

--- app/Filament/Resources/Talks/Tables/TalksTable.php
+++ app/Filament/Resources/Talks/Tables/TalksTable.php
@@ -2,11 +2,18 @@
 
 namespace App\Filament\Resources\Talks\Tables;
 
+use App\Models\Talk;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Schemas\Components\Icon;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\TextInputColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
 
 class TalksTable
 {
@@ -14,18 +21,28 @@ class TalksTable
     {
         return $table
             ->columns([
-                TextColumn::make('title')
-                    ->searchable(),
+                TextInputColumn::make('title')
+                    ->searchable()
+                    ->sortable()
+                    ->rules(['required','max:255'])
+                    ,
+                 ImageColumn::make('speaker.avatar')
+                 ->circular()
+                 ->label('Speaker Avatar')
+                 ->defaultImageUrl(function($record){
+                      return 'https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=' . urlencode($record->speaker->name);
+                 }),
                 TextColumn::make('speaker.name')
-                    ->searchable(),
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->searchable()
+                    ->sortable(),
+                ToggleColumn::make('new_talk'),
+                TextColumn::make('status')
+                ->badge()
+                ->sortable()
+                ->color(function($state){
+                        return $state->getColor();
+                }),
+               
             ])
             ->filters([
                 //
