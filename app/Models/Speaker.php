09d119d871703b98ecb67d7f850b4282236df8bc
mariam <mariamemampp00@gmail.com>
2026-03-19 13:34:58 +0200

    refactored the qualifications in speaker model

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -10,10 +10,24 @@ use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Speaker extends Model
 {
     use HasFactory;
+
+    const QUALIFICATIONS = [
+        'business-leader' => 'Business Leader',
+        'charisma' => 'Charismatic Speaker',
+        'first-time' => 'First Time Speaker',
+        'hometown-hero' => 'Hometown Hero',
+        'humanitarian' => 'Works in Humanitarian Field',
+        'laracasts-contributor' => 'Laracasts Contributor',
+        'twitter-influencer' => 'Large Twitter Following',
+        'youtube-influencer' => 'Large YouTube Following',
+        'open-source' => 'Open Source Creator / Maintainer',
+        'unique-perspective' => 'Unique Perspective'
+    ];
 
     protected function casts(): array
     {
@@ -27,6 +41,14 @@ class Speaker extends Model
     {
         return $this->belongsToMany(Conference::class);
     }
+
+
+    public function talks(): HasMany
+    {
+        return $this->hasMany(Talk::class);
+    }
+
+
     public static function getForm():array
     { 
          return [
@@ -49,18 +71,7 @@ class Speaker extends Model
                 CheckboxList::make('qualifications')  
                 ->searchable()
                 ->columnSpanFull()
-                ->options([
-                        'business-leader' => 'Business Leader',
-                        'charisma' => 'Charismatic Speaker',
-                        'first-time' => 'First Time Speaker',
-                        'hometown-hero' => 'Hometown Hero',
-                        'humanitarian' => 'Works in Humanitarian Field',
-                        'laracasts-contributor' => 'Laracasts Contributor',
-                        'twitter-influencer' => 'Large Twitter Following',
-                        'youtube-influencer' => 'Large YouTube Following',
-                        'open-source' => 'Open Source Creator / Maintainer',
-                        'unique-perspective' => 'Unique Perspective'
-                    ])
+                 ->options(self::QUALIFICATIONS)
                     ->descriptions([ 'business-leader' => 'Business Leader',
                         'charisma' => 'Charismatic Speaker'])
                         
